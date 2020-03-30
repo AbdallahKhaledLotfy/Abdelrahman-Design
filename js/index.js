1daeccf2e25316011ff799c0735205a7a9d0d877
AbdallahKhaledLotfy <62547732+AbdallahKhaledLotfy@users.noreply.github.com>
2020-03-30 06:56:53 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,153 @@
+$(document).ready(function (e) {
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclick($win, $navbar, width);
+
+    // resize event
+    $win.resize(function () {
+        toggle_onclick($win, $navbar, width);
+    });
+
+    $toggle.click(function (e) {
+        $navbar.toggleClass("toggle-left");
+    })
+
+});
+
+
+function toggle_onclick($win, $navbar, width) {
+    if ($win.width() <= 425) {
+        $navbar.css({
+            left: `-${width}px`
+        });
+    } else {
+        $navbar.css({
+            left: '0px'
+        });
+    }
+}
+
+var typed = new Typed('#typed', {
+    strings: [
+        'graphic designer',
+        'Freelancer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+var typed_2 = new Typed('#typed_2', {
+    strings: [
+        'graphic designer',
+        'Freelancer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+
+
+    /*Start animation */
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
+});
+
+function counter() {
+    var count = setInterval(function () {
+        var c = parseInt($('.counter').text());
+        $('.counter').text((++c).toString());
+        if (c == 100) {
+            clearInterval(count);
+            $('.counter').addClass('hide')
+            $('.preloader').addClass('active')
+        }
+    }, 50)
+}
+counter()
+
+
+
+$(document).ready(function () {
+    $('.toggle-black').click(function () {
+        $('.toggle-black').toggleClass('active')
+        $('body').toggleClass('night')
+
+
+    })
+})
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    if (window.pageYOffset > 300) { // Show backToTopButton
+        if (!backToTopButton.classList.contains("btnEntrance")) {
+            backToTopButton.classList.remove("btnExit");
+            backToTopButton.classList.add("btnEntrance");
+            backToTopButton.style.display = "block";
+        }
+    } else { // Hide backToTopButton
+        if (backToTopButton.classList.contains("btnEntrance")) {
+            backToTopButton.classList.remove("btnEntrance");
+            backToTopButton.classList.add("btnExit");
+            setTimeout(function () {
+                backToTopButton.style.display = "none";
+            }, 250);
+        }
+    }
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+// function backToTop() {
+//   window.scrollTo(0, 0);
+// }
+
+function smoothScrollBackToTop() {
+    const targetPosition = 0;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 750;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+        if (!start) start = timestamp;
+        const progress = timestamp - start;
+        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+        if (progress < duration) window.requestAnimationFrame(step);
+    }
+}
+
+function easeInOutCubic(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t * t + b;
+    t -= 2;
+    return c / 2 * (t * t * t + 2) + b;
+};
